7ff73ad678e5f21d748d095680d35653db3f1528
Antoine <antoine.turquetil@telecom-paristech.fr>
2016-06-12 16:43:28 +0200

    Added one option to main.cpp makefile to link with boost regex

--- test/main.cpp
+++ test/main.cpp
@@ -36,117 +36,86 @@ int main(void)
 	vector<vector<string> > x_train;
 	vector<bool> y_train;
 	vector<vector<string> > x_test;
-	loaders::load_train("../data/train.csv",x_train,y_train);
-	loaders::load_test("../data/test.csv",x_test);
 	
-	unordered_set<string> stopwords;
-	fstream stopfile("stopwords.txt",fstream::in);
-	string stopword;
-	while(stopfile >> stopword)
-		stopwords.insert(stopword);
+	loaders::load_data("../data/train.csv","../data/test.csv",x_train,y_train,x_test);
 	
-	// It works, bitches.
-// 	for(auto s : x_train[29])
-// 		cout << s << endl;
-// 	for(auto s : x_test[29])
-// 		cout << s << endl;
+	cout << x_train.size() << endl;
+	for(int i=0;i<10;i++)
+		cout << y_train[i] << " ";
+	cout << endl;
 	
-// 	vector<unordered_map<string,int> > x_train2 = vectorizers::count_vectorize(x_train,stopwords);
-	
-// 	sparsem x_train2 = vectorizers::bin_vectorize(x_train);
-// 	int n_features = x_train2.size2();
-// 	bernouilli_naive_bayes clf(0.5);
-// 	int taken_samples = 3800,n = x_train2.size1();
-// 	sparsem x_train3 = boost::numeric::ublas::subrange(x_train2, 0,taken_samples, 0,n_features);
-// 	sparsem x_valid = boost::numeric::ublas::subrange(x_train2, taken_samples,n, 0,n_features);
+// 	loaders::load_train("../data/train.csv",x_train,y_train);
+// 	loaders::load_test("../data/test.csv",x_test);
 // 	
-// 	vector<bool> y_train3(y_train.begin(),y_train.begin() + taken_samples);
-// 	vector<bool> y_valid(y_train.begin() + taken_samples,y_train.end());
-// 	clf.fit(x_train3,y_train3);
-// 	std::vector<bool> y_pred = clf.predict(x_valid);
-// 	cout << utils::score(y_pred,y_valid) << endl;
+// 	unordered_set<string> stopwords;
+// 	fstream stopfile("stopwords.txt",fstream::in);
+// 	string stopword;
+// 	while(stopfile >> stopword)
+// 		stopwords.insert(stopword);
 	
-// 	vector<unordered_map<string,int> > x_train2 = vectorizers::n_gram_vectorize(x_train,grams);
-// 	for(auto p : x_train2[29])
-// 			cout << p.first << endl;
 	
-	////// Tentative of Naïve Bayes //////
-	
-	// Seems like for now the only way to make the classifier work better than random is by tuning alpha = 0...
-	
+// 	sp_mat A = vectorizers::bin_vectorize(x_train);
+// 	cout << size(A) << endl;
 // 	
-// 	vector<unordered_map<string,int> > x_train3(x_train2.begin(),x_train2.begin() + taken_samples);
+// 	int n_samples = A.n_rows,n = (n_samples*9)/10,p = A.n_cols,n_valid = n_samples - n;
+// 	sp_mat X = A.head_rows(n);
+// 	vector<double> y;
+// 	for(bool x : y_train)
+// 		y.push_back (x ? 1. : -1.);
+// 	vec Y(vector<double>(y.begin(),y.begin() + n));
+// 	clock_t start;
+// 
+// 	start = clock();
 // 	
-// 	vector<unordered_map<string,int> > x_valid(x_train2.begin() + taken_samples,x_train2.end());
+// 	double w0 = 0.,rho = 0.2,alpha = 0.002;
+// 	vec w = zeros<vec>(p);
+// // 	cout << "lolz" << endl;
+// 	auto cur_grad = grad(X,Y,w,w0,rho);
+// // 	cout << "hey" << endl;
+// 	double norm_grad = sqrt(cur_grad.first*cur_grad.first + sum(cur_grad.second%cur_grad.second));
+// 	double eps = 5e-2;
+// 	while(norm_grad > eps)
+// 	{
+// // 		cout << norm_grad << endl;
+// // 		cout << cur_grad.second.head(10) << endl;
+// 		w0 -= alpha*cur_grad.first;
+// 		w -= alpha*cur_grad.second;
+// 		cur_grad = grad(X,Y,w,w0,rho);
+// 		norm_grad = sqrt(cur_grad.first*cur_grad.first + sum(cur_grad.second%cur_grad.second));
+// 	}
+// 	cout << "Done in " << ( clock() - start ) / (double) CLOCKS_PER_SEC << " secondes." << endl;
+// 	sp_mat X_valid = A.tail_rows(n_samples - n);
+// 	vec Y_valid(vector<double>(y.begin() + n,y.end()));
+// // 	double x = 0.;
+// // 	for(double b : y)
+// // 		x += (b == -1.);
+// // 	cout << x/n << endl;
+// // 	
+// 	vec Y_pred = 1./(1. + exp(- w0*ones<vec>(n_valid) - X_valid*w));
+// // 	vec Y_pred = 1./(1. + exp(-X_valid*w));
 // 	
-// 	naive_bayes clf(0.5);
-// 	clf.fit2(x_train3,y_train3);
-// 	auto y_pred = clf.predict3(x_valid);
+// // 	auto Z = X_valid*w;
+// // 	cout << Z.head_rows(10) << endl;
+// // 	cout << Z.head(10) << endl;
+// // 	cout << Y_pred.head(10) << endl;
+// // 	cout << Y_validss.head(10) << endl;
+// 	double score = 0.,score2 = 0;
+// 	double seuil = 0.3111; // 0.3111 is near optimal
+// 	// Le meilleur seuil est étrangement bas, j'ai du me tromper dans la fonction de prédiction.
+// 	// En fait, c'est l'intercept qui cause ça. w0 ne fait que décaler le seuil de toute façon, il n'a pas de réel intérêt pour ce problème.
+// 	for(int i=0;i<(int)n_valid;i++)
+// 	{
+// 		score2 += Y_valid[i] == -1.;
+// // 		if(Y_pred[i] > 0.5)
+// // 			cout << i << endl;
+// 		score += ((Y_pred[i] > seuil && Y_valid[i] == 1.) || (Y_pred[i] <= seuil && Y_valid[i] == -1.));
+// 	}
+// 	cout << score/n_valid << endl;
 // 	
-// 	cout << utils::score(y_pred,y_valid) << endl;
-	
-	sp_mat A = vectorizers::bin_vectorize(x_train);
-	cout << size(A) << endl;
-	
-	int n_samples = A.n_rows,n = (n_samples*9)/10,p = A.n_cols,n_valid = n_samples - n;
-	sp_mat X = A.head_rows(n);
-	vector<double> y;
-	for(bool x : y_train)
-		y.push_back (x ? 1. : -1.);
-	vec Y(vector<double>(y.begin(),y.begin() + n));
-	clock_t start;
-
-	start = clock();
-	
-	double w0 = 0.,rho = 0.2,alpha = 0.002;
-	vec w = zeros<vec>(p);
-// 	cout << "lolz" << endl;
-	auto cur_grad = grad(X,Y,w,w0,rho);
-// 	cout << "hey" << endl;
-	double norm_grad = sqrt(cur_grad.first*cur_grad.first + sum(cur_grad.second%cur_grad.second));
-	double eps = 5e-2;
-	while(norm_grad > eps)
-	{
-// 		cout << norm_grad << endl;
-// 		cout << cur_grad.second.head(10) << endl;
-		w0 -= alpha*cur_grad.first;
-		w -= alpha*cur_grad.second;
-		cur_grad = grad(X,Y,w,w0,rho);
-		norm_grad = sqrt(cur_grad.first*cur_grad.first + sum(cur_grad.second%cur_grad.second));
-	}
-	cout << "Done in " << ( clock() - start ) / (double) CLOCKS_PER_SEC << " secondes." << endl;
-	sp_mat X_valid = A.tail_rows(n_samples - n);
-	vec Y_valid(vector<double>(y.begin() + n,y.end()));
-// 	double x = 0.;
-// 	for(double b : y)
-// 		x += (b == -1.);
-// 	cout << x/n << endl;
-// 	
-	vec Y_pred = 1./(1. + exp(- w0*ones<vec>(n_valid) - X_valid*w));
-// 	vec Y_pred = 1./(1. + exp(-X_valid*w));
-	
-// 	auto Z = X_valid*w;
-// 	cout << Z.head_rows(10) << endl;
-// 	cout << Z.head(10) << endl;
-// 	cout << Y_pred.head(10) << endl;
-// 	cout << Y_validss.head(10) << endl;
-	double score = 0.,score2 = 0;
-	double seuil = 0.3111; // 0.3111 is near optimal
-	// Le meilleur seuil est étrangement bas, j'ai du me tromper dans la fonction de prédiction.
-	// En fait, c'est l'intercept qui cause ça. w0 ne fait que décaler le seuil de toute façon, il n'a pas de réel intérêt pour ce problème.
-	for(int i=0;i<(int)n_valid;i++)
-	{
-		score2 += Y_valid[i] == -1.;
-// 		if(Y_pred[i] > 0.5)
-// 			cout << i << endl;
-		score += ((Y_pred[i] > seuil && Y_valid[i] == 1.) || (Y_pred[i] <= seuil && Y_valid[i] == -1.));
-	}
-	cout << score/n_valid << endl;
-	
-	cout << score2/n_valid << endl;
-// 	cout << norm_grad << endl;
-// 	cout << 
-// 	cout << cur_grad.second.head(10) << endl;
-	//
+// 	cout << score2/n_valid << endl;
+// // 	cout << norm_grad << endl;
+// // 	cout << 
+// // 	cout << cur_grad.second.head(10) << endl;
+// 	//
 	return 0;
 }
---
+++ test/main_archive.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+#include"easySD.hpp"
+#include<unordered_set>
+#include<algorithm>
+#include<armadillo>
+#include<cmath>
+#include<cstdio>
+#include<cmath>
+
+using namespace std;
+using namespace arma;
+
+vector<int> grams = {1};
+
+// 12/06 : This code shows our first logistic regression without intercept. We managed to do the same as scikit.
+
+double val(const sp_mat& X,const vec& Y,const vec& w,const double& C)
+{
+	int n = X.n_rows;
+	vec exp_vect = exp(-Y%(X*w));
+	return C*sum(log(1. + exp_vect))/n + sum(w%w)/2.;
+}
+
+vec grad(const sp_mat& X,const vec& Y,const vec& w,const double& C)
+{
+	int n = X.n_rows;
+	// 	cout << "lolol" << endl;
+	vec exp_vect = exp(-Y%(X*w));
+	// 	cout << "yolo" << endl;
+	vec coefs = -Y%exp_vect/(1. + exp_vect);
+	// 	cout << "merde" << endl;
+	return C*(coefs.t()*X/n).t() + w;
+}
+
+int main(void)
+{
+	cout << "YOLO" << endl;
+	vector<vector<string> > x_train;
+	vector<bool> y_train;
+	vector<vector<string> > x_test;
+	loaders::load_train("../data/train.csv",x_train,y_train);
+	loaders::load_test("../data/test.csv",x_test);
+	
+	unordered_set<string> stopwords;
+	fstream stopfile("stopwords.txt",fstream::in);
+	string stopword;
+	while(stopfile >> stopword)
+		stopwords.insert(stopword);
+	
+	sp_mat A = vectorizers::bin_vectorize(x_train);
+	cout << size(A) << endl;
+	
+	int n_samples = A.n_rows,n = (n_samples*9)/10,p = A.n_cols,n_valid = n_samples - n;
+	sp_mat X = A.head_rows(n);
+	vector<double> y;
+	for(bool x : y_train)
+		y.push_back (x ? 1. : -1.);
+	vec Y(vector<double>(y.begin(),y.begin() + n));
+	clock_t start;
+	
+	start = clock();
+	
+	double seuil = 0.5; // we get 0.773756 accuracy. We do the same with scikit if we properly set class_set = class_weight={1 : np.sum(y_train)/n, 0 : 1 - np.sum(y_train)/n}
+	double C = 1.,alpha = 0.002;
+	vec w = zeros<vec>(p);
+	// 	cout << "lolz" << endl;
+	vec cur_grad = grad(X,Y,w,C);
+	// 	cout << "hey" << endl;
+	double norm_grad = norm(cur_grad);
+	double eps = 1e-4;
+	while(norm_grad > eps)
+	{
+		// 		cout << norm_grad << endl;
+		// 		cout << cur_grad.second.head(10) << endl;
+		w -= alpha*cur_grad;
+		cur_grad = grad(X,Y,w,C);
+		norm_grad = norm(cur_grad);
+	}
+	cout << "Done in " << ( clock() - start ) / (double) CLOCKS_PER_SEC << " secondes." << endl;
+	sp_mat X_valid = A.tail_rows(n_samples - n);
+	vec Y_valid(vector<double>(y.begin() + n,y.end()));
+	// 	double x = 0.;
+	// 	for(double b : y)
+	// 		x += (b == -1.);
+	// 	cout << x/n << endl;
+	// 	
+	vec Y_pred = 1./(1. + exp(-X_valid*w));
+	// 	vec Y_pred = 1./(1. + exp(-X_valid*w));
+	
+	// 	auto Z = X_valid*w;
+	// 	cout << Z.head_rows(10) << endl;
+	// 	cout << Z.head(10) << endl;
+	// 	cout << Y_pred.head(10) << endl;
+	// 	cout << Y_validss.head(10) << endl;
+	double score = 0.,score2 = 0;
+	for(int i=0;i<(int)n_valid;i++)
+	{
+		score2 += Y_valid[i] == -1.;
+		// 		if(Y_pred[i] > 0.5)
+		// 			cout << i << endl;
+		score += ((Y_pred[i] > seuil && Y_valid[i] == 1.) || (Y_pred[i] <= seuil && Y_valid[i] == -1.));
+	}
+	cout << score/n_valid << endl;
+	
+	cout << score2/n_valid << endl;
+	// 	cout << norm_grad << endl;
+	// 	cout << 
+	// 	cout << cur_grad.second.head(10) << endl;
+	//
+	return 0;
+}
