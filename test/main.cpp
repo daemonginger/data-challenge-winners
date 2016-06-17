8e210060d08ebcfa5bb2a3e1877a0748b08cf921
Antoine <antoine.turquetil@telecom-paristech.fr>
2016-06-17 08:15:21 +0200

    Fixed a few things in comments/code

--- test/main.cpp
+++ test/main.cpp
@@ -18,21 +18,23 @@ int main(void)
 	vector<vector<string> > x_train;
 	vector<bool> y_train;
 	vector<vector<string> > x_test;
-	loaders::load_data("../data/train.csv","../data/test.csv",x_train,y_train,x_test);
+	loaders::load_data("../data/train.csv","../data/test.csv",x_train,y_train,x_test,5);
 	
-// 	sp_mat A = vectorizers::count_vectorize(x_train,grams);
-    sp_mat A = vectorizers::tfidf_vectorize(x_train,grams);
-    
+	sp_mat A = vectorizers::count_vectorize(x_train,grams);
+	
 	int n_samples = A.n_rows,n = (n_samples*9)/10,n_valid = n_samples - n;
 	sp_mat X_train = A.head_rows(n), X_valid = A.tail_rows(n_valid);
 	vec Y_train(vector<double>(y_train.begin(),y_train.begin() + n)),Y_valid(vector<double>(y_train.begin() + n,y_train.end()));
 	
-	// n-grams works but our gradient descent in logreg is not fast enough for very large matrices.
-	// we will probably implement linear search in the near future.
-	double C = 10.,tol = 1e-5,alpha = 0.0002;
+	cout << "Number of samples in train : " << X_train.n_rows << endl << "Number of features (ie size of dictionnary) : " << X_train.n_cols << endl;
+	
+	// Logreg with 2-grams didn't work as good as on scikit-kit, we need to understand why.
+	double C = 1.,tol = 1e-5,alpha = 0.00002;
 	logreg clf = logreg(C,tol,alpha);
 	clf.verbose = true;
 	clf.fit(X_train,Y_train);
+	cout << clf.w.head(10) << endl;
+	cout << clf.w0 << endl;
 	cout << "Score on validation : " << clf.score(X_valid,Y_valid) << endl;
 	
 	return 0;
